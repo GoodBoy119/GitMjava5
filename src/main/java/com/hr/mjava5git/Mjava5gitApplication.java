c9c9f7acb9cb93d7dc23cb0582f8463a0fc5e6b1
Forever <2575004352@qq.com>
2020-06-15 20:39:47 +0800

    第一次写bug

---
+++ src/main/java/com/hr/mjava5git/Mjava5gitApplication.java
@@ -0,0 +1,13 @@
+package com.hr.mjava5git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mjava5gitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mjava5gitApplication.class, args);
+    }
+
+}
